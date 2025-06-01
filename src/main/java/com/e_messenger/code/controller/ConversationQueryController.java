1c4e646942ece374a1be7401f1fa62ef845c78b8
Hecker - Chuối <nguyentienht1910@gmail.com>
2025-06-01 22:12:17 +0700

    feat: search conversation by key
    Xóa get recent contact (có thể dùng get all direct thay thế)
    Thêm api search conversation dùng tham số key

--- src/main/java/com/e_messenger/code/controller/ConversationQueryController.java
+++ src/main/java/com/e_messenger/code/controller/ConversationQueryController.java
@@ -2,13 +2,9 @@ package com.e_messenger.code.controller;
 
 import com.e_messenger.code.dto.responses.ApiResponse;
 import com.e_messenger.code.dto.responses.ConversationResponse;
-import com.e_messenger.code.dto.responses.UserResponse;
 import com.e_messenger.code.entity.Conversation;
-import com.e_messenger.code.entity.Participant;
-import com.e_messenger.code.entity.User;
 import com.e_messenger.code.mapstruct.ConversationMapper;
-import com.e_messenger.code.mapstruct.UserMapper;
-import com.e_messenger.code.service.ConversationQueryService;
+import com.e_messenger.code.service.impl.ConversationQueryServiceImpl;
 import com.e_messenger.code.service.impl.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AccessLevel;
@@ -16,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -24,12 +21,12 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @SecurityRequirement(name = "user token")
 public class ConversationQueryController {
-    ConversationQueryService mainService;
+    ConversationQueryServiceImpl mainService;
     ConversationMapper conversationMapper;
-    private final UserService userService;
+    UserService userService;
 
     @GetMapping("/direct/{otherId}")
-    public ApiResponse<ConversationResponse> getDirectChat(@PathVariable String otherId){
+    public ApiResponse<ConversationResponse> getDirectChat(@PathVariable String otherId) {
         Conversation result = mainService.getDirectChat(otherId);
         return ApiResponse.<ConversationResponse>builder()
                 .result(conversationMapper.toResponse(result))
@@ -37,7 +34,7 @@ public class ConversationQueryController {
     }
 
     @GetMapping("/{conversationId}")
-    public ApiResponse<ConversationResponse> getConversation(@PathVariable String conversationId){
+    public ApiResponse<ConversationResponse> getConversation(@PathVariable String conversationId) {
         Conversation result = mainService.getConversationById(conversationId, userService.getCurrentUser().getId());
         return ApiResponse.<ConversationResponse>builder()
                 .result(conversationMapper.toResponse(result))
@@ -47,7 +44,7 @@ public class ConversationQueryController {
     @GetMapping("/direct")
     public ApiResponse<List<ConversationResponse>> getAllDirectChat(
             @RequestParam(defaultValue = "0") int pageNum,
-            @RequestParam(defaultValue = "10") int pageSize){
+            @RequestParam(defaultValue = "10") int pageSize) {
         List<Conversation> result = mainService.getAllDirectChat(pageNum, pageSize);
         return ApiResponse.<List<ConversationResponse>>builder()
                 .result(conversationMapper.toResponses(result))
@@ -58,7 +55,7 @@ public class ConversationQueryController {
     public ApiResponse<List<ConversationResponse>> getAllGroupChat(
             @RequestParam(defaultValue = "0") int pageNum,
             @RequestParam(defaultValue = "10") int pageSize
-    ){
+    ) {
         List<Conversation> result = mainService.getAllGroupChat(pageNum, pageSize);
         return ApiResponse.<List<ConversationResponse>>builder()
                 .result(conversationMapper.toResponses(result))
@@ -69,8 +66,21 @@ public class ConversationQueryController {
     public ApiResponse<List<ConversationResponse>> getAllConversation(
             @RequestParam(defaultValue = "0") int pageNum,
             @RequestParam(defaultValue = "10") int pageSize
-    ){
+    ) {
         List<Conversation> result = mainService.getAllConversation(pageNum, pageSize);
+        return ApiResponse.<List<ConversationResponse>>builder()
+                .result(conversationMapper.toResponses(result))
+                .build();
+    }
+
+    @GetMapping("/search/{key}")
+    public ApiResponse<List<ConversationResponse>> searchConversationByName(
+            @PathVariable String key,
+            @RequestParam(defaultValue = "0") int pageNum,
+            @RequestParam(defaultValue = "10") int pageSize,
+            Principal principal) {
+        List<Conversation> result = mainService.searchConversation(key, pageNum, pageSize, principal);
+
         return ApiResponse.<List<ConversationResponse>>builder()
                 .result(conversationMapper.toResponses(result))
                 .build();
